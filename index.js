884900d064e9dd361a64b6494722d0e5be22adfb
iency <pwnzjake@gmail.com>
2022-07-08 10:54:05 +0300

    commit

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-// template_68uqrxi
-// service_2jnpyc4
-// wW6f-kxXd0t98LmQG
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            'service_2jnpyc4',
+            'template_68uqrxi',
+            event.target,
+            'wW6f-kxXd0t98LmQG'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailble. Please contact me directly on pwnzjake@gmail.com"
+        );
+    })
+}
+
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
